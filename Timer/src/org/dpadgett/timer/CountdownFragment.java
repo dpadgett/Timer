235c519389b5d666bcd6c4b3623662c91e9d1113
Dan Padgett <dumbledore3@gmail.com>
2012-01-10 09:08:58 -0800

    Make start/stop button toggle the input mode

--- Timer/src/org/dpadgett/timer/CountdownFragment.java
+++ Timer/src/org/dpadgett/timer/CountdownFragment.java
@@ -1,5 +1,6 @@
 package org.dpadgett.timer;
 
+import android.R.attr;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,36 +17,67 @@ import android.widget.TextView;
 
 public class CountdownFragment extends Fragment {
 
+	private boolean inputMode;
+	private LinearLayout inputLayout;
+	private LinearLayout timerLayout;
+	private View rootView;
+	private final Handler handler;
+	
+	public CountdownFragment() {
+		this.inputMode = true;
+		this.handler = new Handler();
+	}
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View rootView = inflater.inflate(R.layout.countdown, container, false);
+        rootView = inflater.inflate(R.layout.countdown, container, false);
+        LinearLayout inputs = (LinearLayout) rootView.findViewById(R.id.inputsLayout);
+        this.inputLayout = (LinearLayout) rootView.findViewById(R.id.inputsInnerLayout);
+        this.timerLayout = createTimerLayout(inputs);
         Button startButton = (Button) rootView.findViewById(R.id.startButton);
-        final Handler handler = new Handler();
         startButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						LinearLayout inputs = (LinearLayout) rootView.findViewById(R.id.inputsLayout);
-						
-						LinearLayout runningLayout = new LinearLayout(inputs.getContext());
-						runningLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0f));
-						runningLayout.setGravity(Gravity.CENTER_HORIZONTAL);
-						runningLayout.setId(R.id.inputsInnerLayout);
-						
-						TextView timerText = new TextView(inputs.getContext());
-						timerText.setText("00:00:00");
-						timerText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50f);
-						
-						inputs.removeAllViews();
-					}
-				});
+				handler.post(new ToggleInputMode());
 			}
         });
         return rootView;
     }
 
+    private class ToggleInputMode implements Runnable {
+
+		@Override
+		public void run() {
+			inputMode = !inputMode;
+			LinearLayout inputs = (LinearLayout) rootView.findViewById(R.id.inputsLayout);
+			Button startButton = (Button) rootView.findViewById(R.id.startButton);
+			if (inputMode) {
+				inputs.removeAllViews();
+				inputs.addView(inputLayout);
+				startButton.setText("Start");
+			} else {
+				inputs.removeAllViews();
+				inputs.addView(timerLayout);
+				startButton.setText("Stop");
+			}
+		}
+    	
+    }
+
+    private static LinearLayout createTimerLayout(LinearLayout inputs) {
+		LinearLayout runningLayout = new LinearLayout(inputs.getContext());
+		runningLayout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0f));
+		runningLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+		runningLayout.setId(R.id.inputsInnerLayout);
+		
+		TextView timerText = new TextView(inputs.getContext());
+		timerText.setText("00:00:00");
+		timerText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50f);
+		timerText.setTextAppearance(inputs.getContext(), attr.textAppearanceLarge);
+		
+		runningLayout.addView(timerText);
+		return runningLayout;
+    }
 }

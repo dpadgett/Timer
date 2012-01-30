b56757f738f62716fed00b5110e87f68ce0ae814
Dan Padgett <dumbledore3@gmail.com>
2012-01-29 21:59:13 -0800

    switch to use ListView, and fix background on add button

--- Timer/src/org/dpadgett/timer/WorldClockFragment.java
+++ Timer/src/org/dpadgett/timer/WorldClockFragment.java
@@ -2,12 +2,15 @@ package org.dpadgett.timer;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 import java.util.TimeZone;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
@@ -16,42 +19,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.DigitalClock;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class WorldClockFragment extends Fragment {
 	
 	private DanResourceFinder finder;
 	private Context context;
 	private Handler uiHandler;
+	private final ClockListAdapter clocksListAdapter;
+    private final List<String> clockList;
 	
-	private interface EasyLayoutInflater {
-		View inflate(int id);
-	}
-	
-	private EasyLayoutInflater inflater;
-
 	public WorldClockFragment() {
+		clockList = new ArrayList<String>();
+		clocksListAdapter = new ClockListAdapter();
 	}
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
     	uiHandler = new Handler();
         View rootView = inflater.inflate(R.layout.world_clock, container, false);
         finder = DanWidgets.finderFrom(rootView);
         context = rootView.getContext();
-        this.inflater = new EasyLayoutInflater() {
-			@Override
-			public View inflate(int id) {
-				return inflater.inflate(id, container, false);
-			}
-        };
         LinearLayout addClockView = (LinearLayout) finder.findViewById(R.id.addClockView);
         addClockView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				view.setBackgroundResource(android.R.drawable.list_selector_background);
+				view.setBackgroundResource(
+						Resources.getSystem().getIdentifier("list_selector_holo_dark", "drawable", "android"));
+						//android.R.drawable.list_selector_background);
 				uiHandler.post(new Runnable() {
 					@Override
 					public void run() {
@@ -60,7 +59,8 @@ public class WorldClockFragment extends Fragment {
 				});
 			}
         });
-        //Resources.getSystem().getIdentifier("list_selector_background", "drawable", "android");
+		ListView clocksList = (ListView) finder.findViewById(R.id.clocksList);
+		clocksList.setAdapter(clocksListAdapter);
         return rootView;
     }
 
@@ -68,39 +68,67 @@ public class WorldClockFragment extends Fragment {
 	 * Adds a new clock to the view
 	 */
 	private void addNewClock() {
-		LinearLayout clocksLayout = (LinearLayout) finder.findViewById(R.id.clocksLayout);
-		LinearLayout newClock = (LinearLayout) inflater.inflate(R.layout.single_world_clock);
-		DigitalClock clock = (DigitalClock) newClock.findViewById(R.id.digitalClock);
 		String[] allIDs = TimeZone.getAvailableIDs();
-		final String myID = allIDs[new Random().nextInt(allIDs.length)];
-		clock.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) { }
+		String myID = allIDs[new Random().nextInt(allIDs.length)];
+		clockList.add(myID);
+		clocksListAdapter.notifyDataSetChanged();
+	}
+	
+	private class ClockListAdapter extends BaseAdapter {
+		 
+	    public ClockListAdapter() {
+	    }
+	 
+	    public int getCount() {
+	        return clockList.size();
+	    }
+	 
+	    public String getItem(int position) {
+	        return clockList.get(position);
+	    }
+	 
+	    public long getItemId(int position) {
+	        return clockList.get(position).hashCode();
+	    }
+	 
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        final String myID = clockList.get(position);
+	 
+	        LinearLayout newClock =
+	        		(LinearLayout) LayoutInflater.from(context)
+	        			.inflate(R.layout.single_world_clock, parent, false);
+	 
+	        DigitalClock clock = (DigitalClock) newClock.findViewById(R.id.digitalClock);
+			clock.addTextChangedListener(new TextWatcher() {
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count) { }
 
-			private boolean isInternalEdit = true;
-			@Override
-			public void afterTextChanged(Editable s) {
-				isInternalEdit = !isInternalEdit;
-				if (!isInternalEdit) {
-					SimpleDateFormat sdf = new SimpleDateFormat("h:mm:ss a");
-					Date newDate = new Date(); // as a fallback
-					try {
-						newDate = sdf.parse(s.toString());
-					} catch (ParseException e) {
+				private boolean isInternalEdit = true;
+				@Override
+				public void afterTextChanged(Editable s) {
+					isInternalEdit = !isInternalEdit;
+					if (!isInternalEdit) {
+						SimpleDateFormat sdf = new SimpleDateFormat("h:mm:ss a");
+						Date newDate = new Date(); // as a fallback
+						try {
+							newDate = sdf.parse(s.toString());
+						} catch (ParseException e) {
+						}
+						sdf.setTimeZone(TimeZone.getTimeZone(myID));
+						String toText = sdf.format(newDate);
+						s.replace(0, s.length(), toText.toLowerCase());
 					}
-					sdf.setTimeZone(TimeZone.getTimeZone(myID));
-					String toText = sdf.format(newDate);
-					s.replace(0, s.length(), toText.toLowerCase());
 				}
-			}
-		});
-		
-		AnalogClockWithTimezone analogClock =
-				(AnalogClockWithTimezone) newClock.findViewById(R.id.analogClock);
-		analogClock.setTimezone(myID);
-		
-		clocksLayout.addView(newClock);
+			});
+			
+			AnalogClockWithTimezone analogClock =
+					(AnalogClockWithTimezone) newClock.findViewById(R.id.analogClock);
+			analogClock.setTimezone(myID);
+	 
+	        return newClock;
+	    }
+	 
 	}
 }
